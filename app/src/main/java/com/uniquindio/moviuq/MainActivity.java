fee8448dbaaedb1fb3170660663e53f0a7564f12
Santiago Florez Estrada <santiago01est@gmail.com>
2022-09-27 10:33:56 -0500

    Diseño version 2.0 activity login Cambios en diseño botones y colores de la barra inferior y superior, asignación de themes a cada activity, se agrego funcionalidad del boton inciar sesion

--- app/src/main/java/com/uniquindio/moviuq/MainActivity.java
+++ app/src/main/java/com/uniquindio/moviuq/MainActivity.java
@@ -1,15 +1,27 @@
 package com.uniquindio.moviuq;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("esto es un cambio");
-        //hola soy nick :D
+    }
+
+    public void lanzarLogin(View view) {
+        Intent i = new Intent(this, Login.class);
+        startActivity(i);
+     //   overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
     }
 }
