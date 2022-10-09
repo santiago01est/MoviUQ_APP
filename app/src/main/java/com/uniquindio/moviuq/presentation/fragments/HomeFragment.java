8e3bf4ba8a2620bfd5ad6470a0a46feb993d4ba1
Santiago Florez Estrada <santiago01est@gmail.com>
2022-10-09 11:41:54 -0500

    diseño parte superior del HomeFragment en donde se anexo elementos como imagen de perfil y texto de bienvenida al usuario

--- app/src/main/java/com/uniquindio/moviuq/presentation/fragments/HomeFragment.java
+++ app/src/main/java/com/uniquindio/moviuq/presentation/fragments/HomeFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.uniquindio.moviuq.R;
 
 
@@ -39,6 +41,30 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View root=inflater.inflate(R.layout.fragment_home, container, false);
 
+        /** Mecanismo del estado del appBar**/
+        final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) root.findViewById(R.id.collapsingHome);
+        AppBarLayout appBarLayout = (AppBarLayout) root.findViewById(R.id.appbarHome);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = true;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbarLayout.setTitle("Buenos dias, Santiago");
+                    collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.ExpandedAppBar);
+                    // toolbar.setBackgroundColor((Color.parseColor("#5F0F40")));
+                    isShow = true;
+                } else if (isShow) {
+                    collapsingToolbarLayout.setTitle(" ");
+
+                    isShow = false;
+                }
+            }
+        });
 
         return root;
     }
