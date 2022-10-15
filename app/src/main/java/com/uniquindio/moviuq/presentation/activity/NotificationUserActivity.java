24e769d3d1bfdd031a480b754197140ef74eb0bf
Nicolas Rodriguez Moreno <nicolas.rodriguezm@uqvirtual.edu.co>
2022-10-14 19:58:54 -0500

    Se realizo el diseño de la activity notificaciones, adicional se creo el card de notificacion

---
+++ app/src/main/java/com/uniquindio/moviuq/presentation/activity/NotificationUserActivity.java
@@ -0,0 +1,15 @@
+package com.uniquindio.moviuq.presentation.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.uniquindio.moviuq.R;
+
+public class NotificationUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_user);
+    }
+}
