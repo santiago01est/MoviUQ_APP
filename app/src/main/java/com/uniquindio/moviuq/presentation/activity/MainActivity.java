643b42a4cdae6444015ffab8c0700d3724a67d7d
Santiago Florez Estrada <santiago01est@gmail.com>
2022-10-22 20:33:37 -0500

    modificacion en la clase HomeActivity v2

--- app/src/main/java/com/uniquindio/moviuq/presentation/activity/MainActivity.java
+++ app/src/main/java/com/uniquindio/moviuq/presentation/activity/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void lanzarLogin(View view) {
-        Intent i = new Intent(this, CreateOfferActivity.class);
+        Intent i = new Intent(this, LoginActivity.class);
         startActivity(i);
         /*
         Intent i = new Intent(this, LoginActivity.class);
