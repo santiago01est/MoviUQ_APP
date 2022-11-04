a258a552bd303d3fa63f9ac3a179af973eb81527
Nicolas Rodriguez Moreno <nicolas.rodriguezm@uqvirtual.edu.co>
2022-11-04 12:25:01 -0500

    Actualizacion de gradle

---
+++ app/src/androidTest/java/com/uniquindio/moviuq/use_case/Case_LogTest.java
@@ -0,0 +1,17 @@
+package com.uniquindio.moviuq.use_case;
+
+import static org.junit.Assert.*;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.junit.Test;
+
+
+public class Case_LogTest {
+
+    @Test
+    public void someTest() {
+
+       // FirebaseAuth firebaseAuthMock= Mockito.mock(FirebaseAuth.class);
+    }
+}
