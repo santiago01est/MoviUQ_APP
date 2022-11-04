b4dd07fa8a7dea46cfd9a7907d36e762a39073ac
Santiago Florez Estrada <santiago01est@gmail.com>
2022-11-04 10:51:36 -0500

    cambio de FirebaseFirestore.getInstance() y servicios Auth

--- app/src/main/java/com/uniquindio/moviuq/data/NotificationImpl.java
+++ app/src/main/java/com/uniquindio/moviuq/data/NotificationImpl.java
@@ -7,6 +7,7 @@ import androidx.annotation.Nullable;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -18,7 +19,7 @@ public class NotificationImpl implements NotificationService{
     public Query getNotificationUser(String emailUser) {
 
         Query query;
-        query=FirebaseCFDBService.getBD().collection("notification").whereEqualTo("emailUserReceiver",emailUser);
+        query= FirebaseFirestore.getInstance().collection("notification").whereEqualTo("emailUserReceiver",emailUser);
 
         return query;
 
--- app/src/main/java/com/uniquindio/moviuq/data/RequestImpl.java
+++ app/src/main/java/com/uniquindio/moviuq/data/RequestImpl.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.uniquindio.moviuq.domain.Request;
 import com.uniquindio.moviuq.provider.services.firebase.FirebaseCFDBService;
 import com.uniquindio.moviuq.use_case.Case_Request;
@@ -16,7 +17,7 @@ public class RequestImpl implements RequestService{
     @Override
     public void createRequest(Request request, String email, Activity activity) {
         case_request=new Case_Request(activity);
-        FirebaseCFDBService.getBD().collection("request").document(request.getId()).set(request).addOnSuccessListener(new OnSuccessListener<Void>() {
+        FirebaseFirestore.getInstance().collection("request").document(request.getId()).set(request).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void unused) {
                 case_request.lanzarHome();
